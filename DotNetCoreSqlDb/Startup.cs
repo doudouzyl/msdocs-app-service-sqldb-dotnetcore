69b3545f56e9d0ce24449b89e9d9ce0bdf7af50f
doudouzyl <doudouzyl@gmail.com>
2023-03-17 21:15:52 +1100

    Change to MySQL

--- DotNetCoreSqlDb/Startup.cs
+++ DotNetCoreSqlDb/Startup.cs
@@ -28,8 +28,10 @@ namespace DotNetCoreSqlDb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //services.AddDbContext<MyDatabaseContext>(options =>
+            //        options.UseSqlServer(Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
             services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+                    options.UseMySql(ServerVersion.AutoDetect(Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")))) ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
